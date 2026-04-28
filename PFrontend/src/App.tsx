034013db6c8425eeb07a5b49e0ca76a3bf5d9b56
adammenkiel <publicprojectsmenkiel@gmail.com>
2026-04-29 00:33:38 +0200

    Frontend template update
    Added footer and links

--- PFrontend/src/App.tsx
+++ PFrontend/src/App.tsx
@@ -10,6 +10,10 @@ import {
 
 import { Button } from "@/components/ui/button";
 import { Card, CardTitle, CardHeader, CardDescription, CardAction, CardContent, CardFooter } from "./components/ui/card";
+import { Link } from "react-router-dom";
+import { Avatar, AvatarFallback, AvatarImage } from "./components/ui/avatar";
+import { SiDiscord, SiFacebook, SiGithub, SiInstagram, SiYoutube } from "@icons-pack/react-simple-icons";
+
 
   export default function App() {
     return (
@@ -56,18 +60,48 @@ import { Card, CardTitle, CardHeader, CardDescription, CardAction, CardContent, 
                   <CardTitle className = "text-2xl">About project</CardTitle>
                   <CardDescription>Project description, freatures and plans</CardDescription>
                 </CardHeader>
-                <CardContent className="text-[16px]">That's project is an interactive panel for minecraft servers which want to share access to some freatures like chat views for every user thoughtout this page. In the future we plan to add some new freatures like money for buy things in itemshop and broadcasts with visibility depending on the role, access to some account settings and so on!</CardContent>
+                <CardContent className="text-[16px]">
+                  That's project is an interactive panel for minecraft servers which want to share access to some freatures like chat views for every user thoughtout this page. In the future we plan to add some new freatures like money for buy things in itemshop and broadcasts with visibility depending on the role, access to some account settings and so on!
+                </CardContent>
               </Card>
               <Card className="flex-1 border-2 hover:border-blue-400 transition-colors duration-300">
                 <CardHeader>
                   <CardTitle className = "text-2xl">About author</CardTitle>
                   <CardDescription>Author description and links</CardDescription>
                 </CardHeader>
-                <CardContent className="text-[16px]">That's panel was created by Adam Menkiel, backend was created in Java + SpringBoot + Netty, frontend is created in Vite + React + TypeScript, that's opensource project which is possible to download at adammenkiel github</CardContent>
+                <CardContent className="text-[16px] flex flex-col">
+                  That's panel was created by Adam Menkiel, backend was created in Java + SpringBoot + Netty, frontend is created in Vite + React + TypeScript, that's opensource project which is possible to download at adammenkiel github.
+                  <div className="flex">
+                    <a href="https://github.com/adammenkiel/JWebPanel" className="mt-2 text-blue-800 text-[16px]">Link to project</a>
+                  </div>
+                  <a href="https://github.com/adammenkiel/" className="text-blue-800 text-[16px]">Link to author's github</a>
+                </CardContent>
               </Card>
             </div>
         </main>
-        <footer className="mt-4">Stopka</footer>
+        {/* Footer */}
+        <footer className = "shadow">
+          <div className="flex flex-col mt-4 items-center justify-center bg-blue-300 h-40">
+            <div className="flex gap-5 pt-4">
+              <div className="group rounded-full bg-white p-2 hover:scale-105">
+                <SiFacebook className="group-hover:text-blue-600 group-hover:scale-105 transition-colors duration-300"/>
+              </div>
+              <div className="group rounded-full bg-white p-2 hover:scale-105">
+                <SiYoutube className="group-hover:text-red-600 group-hover:scale-105 transition-colors duration-300"/>
+              </div>
+              <div className="group rounded-full bg-white p-2 hover:scale-105">
+                <SiDiscord className="group-hover:text-blue-600 group-hover:scale-105 transition-colors duration-300"/>
+              </div>
+              <div className="group rounded-full bg-white p-2 hover:scale-105">
+                <SiInstagram className="group-hover:text-red-600 group-hover:scale-105 transition-colors duration-300"/>
+              </div>
+            </div>
+            <div className="mt-2">
+              JWebPanel - Interactive panel for minecraft servers!
+            </div>
+          </div>
+          <div className="flex justify-center bg-gray-200 w-full mt-auto">Copyright © 2026 All rights reserved.</div>
+        </footer>
       </div>
     );
   }
