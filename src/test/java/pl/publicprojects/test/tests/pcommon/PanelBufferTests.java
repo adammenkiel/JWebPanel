1d5c29fb0a3b2bf7d6af17565d3f04a6c06e86aa
adammenkiel <publicprojectsmenkiel@gmail.com>
2026-08-13 23:03:27 +0200

    Panel Buffer tests update

--- src/test/java/pl/publicprojects/test/tests/pcommon/PanelBufferTests.java
+++ src/test/java/pl/publicprojects/test/tests/pcommon/PanelBufferTests.java
@@ -9,18 +9,29 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PanelBufferTests {
+
+    public void writeString(DataOutputStream stream, String text) throws IOException {
+        stream.writeInt(text.getBytes(StandardCharsets.UTF_8).length);
+        stream.write(text.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public void writeStringList(DataOutputStream stream, List<String> list) throws IOException {
+        stream.writeInt(list.size());
+        for(String elem : list)
+            this.writeString(stream, elem);
+    }
     @Test
     public void testReadString() throws IOException {
         //Arrange
         String exampleString = "Hello world";
         ByteArrayOutputStream str = new ByteArrayOutputStream();
         DataOutputStream out = new DataOutputStream(str);
-        out.writeInt(exampleString.getBytes(StandardCharsets.UTF_8).length);
-        out.write(exampleString.getBytes(StandardCharsets.UTF_8));
+        this.writeString(out, exampleString);
         ByteBuf buf = Unpooled.wrappedBuffer(str.toByteArray());
         PanelBuffer sut = new PanelBuffer(buf);
 
@@ -29,5 +40,22 @@ public class PanelBufferTests {
 
         //Assert
         assertEquals(resultString, exampleString);
+    }
+
+    @Test
+    public void testReadStringList() throws IOException {
+        //Arrange
+        List<String> list = List.of(new String[]{"First", "second"});
+        ByteArrayOutputStream str = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream(str);
+        this.writeStringList(out, list);
+        ByteBuf buf = Unpooled.wrappedBuffer(str.toByteArray());
+        PanelBuffer sut = new PanelBuffer(buf);
+
+        //Act
+        List<String> resultList = sut.readStringList();
+
+        //Assert
+        assertEquals(resultList, list);
     }
 }
